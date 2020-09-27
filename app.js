ec37295b51f77420daf5292a715caad3850a941a
Arif Hidayat <arif.h@mail.ugm.ac.id>
2020-09-27 15:44:06 +0700

    ssl

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/arifhdev.me/fullchain.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/arifhdev.me/privkey.pem', 'utf8');
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
 
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 //add the router
 app.use('/', router);
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80);
-httpsServer.listen(443);
+app.listen(80);
 
 console.log('Running at Port 80');
